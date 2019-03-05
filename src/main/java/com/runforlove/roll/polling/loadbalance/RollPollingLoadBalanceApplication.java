d39eaaed57d6fb2270de3be58e793e298f98fd99
霜魂 <shuanghun@wacai.com>
2019-03-05 18:13:11 +0800

    f

---
+++ src/main/java/com/runforlove/roll/polling/loadbalance/RollPollingLoadBalanceApplication.java
@@ -0,0 +1,12 @@
+package com.runforlove.roll.polling.loadbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollPollingLoadBalanceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RollPollingLoadBalanceApplication.class, args);
+	}
+}
